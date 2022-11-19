fe97e0cafec0140cf6c0c3c3bd9fc59bb3746d22
Xavi1594 <xavi9415@hotmail.com>
2022-11-19 11:40:14 +0100

    segundo ejercicio

---
+++ src/ex1/ex 2/ex2.js
@@ -0,0 +1,18 @@
+// 
+// * Conectar archivo javascript al html de destino
+// definir el espacio del mensaje
+// acceder al contenedor desde js
+// establecer las variables
+// crear la funcion/
+ 
+
+let texto = "la suma de 3 + 5 es "
+suma();
+document.write(texto + resultado);
+
+function suma(){
+    let numero1=3;
+    let numero2=5;
+
+    resultado= numero1 + numero2;
+}
